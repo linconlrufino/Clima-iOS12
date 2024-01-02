1f68375a55d0c5e8de03c06e5e704315c1e8d8d3
Linconl Rufino <linconldasilvarufino@gmail.com>
2024-01-01 23:21:38 -0300

    location manager methods

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -13,7 +13,7 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
+    let APP_ID = ""
     /***Get your own App ID at https://openweathermap.org/appid ****/
 
 
@@ -35,6 +35,7 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
         locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
         
         
     }
@@ -78,11 +79,29 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     
     //Write the didUpdateLocations method here:
-    
-    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            
+            if location.horizontalAccuracy > 0 {
+                locationManager.stopUpdatingLocation()
+                
+                print("Longitude = \(location.coordinate.longitude), Latitude = \(location.coordinate.latitude)")
+                
+                let latitude = String(location.coordinate.latitude)
+                let longitude = String(location.coordinate.longitude)
+
+                let params: Dictionary<String,String> = ["lat" : latitude, "lon" : longitude, "appid" : APP_ID]
+                
+            }
+            
+        }
+    }
     
     //Write the didFailWithError method here:
-    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        cityLabel.text = "Location Unavailable"
+    }
     
     
 
